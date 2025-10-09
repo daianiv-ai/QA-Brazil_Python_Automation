9610e9cc9c2a3cc7fe53db8a770c6d9922251528
Daiani_v <daianiv@unochapeco.edu.br>
2025-10-08 23:16:53 -0300

    Subindo Projeto Sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-12dcfaf7-02f9-4f77-a84a-726605f72236.containerhub.tripleten-services.com?lng=pt'# Arquivo com constantes
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = 	'1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
